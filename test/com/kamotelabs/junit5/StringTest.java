d5cc892bd4ca7ffa956a23dea48c7a3c101f4c88
Marlon <martianworm17@gmail.com>
2018-04-22 18:42:15 +0800

    Step 02 - First Junit Test - Red bar

---
+++ test/com/kamotelabs/junit5/StringTest.java
@@ -0,0 +1,14 @@
+package com.kamotelabs.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
