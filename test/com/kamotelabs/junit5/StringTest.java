493f8a882f52b4dfada56ee7faf2c7c910c6dcb6
Marlon <martianworm17@gmail.com>
2018-04-26 21:16:42 +0800

    Step 07 - Setting up data for every test

--- test/com/kamotelabs/junit5/StringTest.java
+++ test/com/kamotelabs/junit5/StringTest.java
@@ -2,45 +2,51 @@ package com.kamotelabs.junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StringTest {
+
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("Initialize Test Data for each test.");
+	}
 
 	@Test
 	void lengthBasicTest() {
 		int actualLength = "ABCD".length();
 		int expectedLength = 4;
-		
+
 		assertEquals(expectedLength, actualLength);
 		// Assert length == 4
 		// Write test code
 		// Invoke method square(4) Code under test
 		// Checks in place - 16 => Assertions
 	}
-	
+
 	@Test
 	void toUpperCaseTest() {
 		String str = "abcd";
-		
+
 		assertEquals("ABCD", str.toUpperCase());
 		assertNotNull(str.toUpperCase());
 		// assertNull(result);
 	}
-	
+
 	@Test
 	void containsBasic() {
 		String str = "abcdefgh";
-		
+
 		assertEquals(false, str.contains("cddef"));
 		assertFalse(str.contains("cddef"));
 	}
-	
+
 	@Test
 	void splitBasic() {
 		String str = "abc def ghi";
 		String[] actualResult = str.split(" ");
-		String[] expectedResult = new String[] {"abc", "def", "ghi"};
-		
+		String[] expectedResult = new String[] { "abc", "def", "ghi" };
+
 		assertArrayEquals(actualResult, expectedResult);
 	}
 
