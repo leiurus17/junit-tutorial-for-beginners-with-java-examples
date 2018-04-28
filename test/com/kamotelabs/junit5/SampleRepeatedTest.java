830901469df7bc0594e32a03147c6d671efb90ec
Marlon <martianworm17@gmail.com>
2018-04-28 09:59:07 +0800

    Step 14 - Tip - Repeat same test multiple times

--- test/com/kamotelabs/junit5/SampleRepeatedTest.java
+++ test/com/kamotelabs/junit5/SampleRepeatedTest.java
@@ -3,11 +3,9 @@ package com.kamotelabs.junit5;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 public class SampleRepeatedTest {
 	
-	@Test
 	@RepeatedTest(10)
 	void containsBasicRepeat() {
 		assertFalse("abcdefgh".contains("ijk"));
